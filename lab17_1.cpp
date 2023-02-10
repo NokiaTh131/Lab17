92c60b74ae511e276e9d7c61e0db4a5434b6d008
NokiaTh131 <106575326+NokiaTh131@users.noreply.github.com>
2023-02-10 18:36:02 +0700

    Update lab17_1.cpp

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,41 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *dPtr,int N,int M) {
+	cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(dPtr + i);
+        if(i % M == M-1) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *dPtr,int N,int M) {
+    for(int i = 0; i < N*M; i++){
+        *(dPtr+i) = (rand() % 101)/100.0;
+    }
+}
+
+void findRowSum(const double *dPtr,double *sum1,int N,int M) {
+    
+	for(int i = 0; i < N; i++){
+	    double sum = 0;
+        for(int j = 0; j < M; j++){
+			sum += *(dPtr);
+			dPtr++;
+    	}
+    	sum1[i] = sum;
+    }
+}
+
+void findColSum(const double *dPtr,double *sum1,int N,int M) {
+    
+    for(int j = 0; j < M; j++){
+        double sum = 0;
+        for(int i = 0; i < N; i++){
+			sum += dPtr[i * M + j];
+    	}
+    	sum1[j] = sum;
+    }
+}
